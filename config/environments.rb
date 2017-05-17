c8fc3d47fb3d20f481fbd5abdf87f3bffb3ecaef
ismaelnoble <ismaelnoble@gmail.com>
2017-05-17 16:54:23 +0800

    bug fix in ui

--- config/environments.rb
+++ config/environments.rb
@@ -5,10 +5,9 @@ configure :development do
 
   require 'mock_redis'
   REDIS_CONNECTION = MockRedis.new
-  
+
   def reload!
     # Tux reloading tip: https://github.com/cldwalker/tux/issues/3
     exec $PROGRAM_NAME, *ARGV
   end
 end
-
